b132a7f3731e493d46c261d20abbedeaf5d3834c
Makpal Sagidollayeva <35831700+makpal0101@users.noreply.github.com>
2018-03-09 14:40:47 +0600

    Update State.java

--- src/main/java/com/example/examplestatemachine/State.java
+++ src/main/java/com/example/examplestatemachine/State.java
@@ -1,12 +1,18 @@
 package com.example.examplestatemachine;
 
 public abstract class State {
+	
     protected StateContext sc;
     protected boolean accept = false;
+    protected boolean isActionA= false;
     
-    public void actionA() {}
+    public void actionA() {
+        isActionA= true;
+    }
     
-    public void actionB() {}
+    public void actionB() {
+        isActionA= false;
+    }
     
     public boolean isAccept() {
         return this.accept;
